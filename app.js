d8649e31c6f9ff9a9643f7f9d40e753bccbdc036
Mateusz Gibula <gibulam94@gmail.com>
2019-07-30 11:52:31 +0200

    cors and helmet

--- app.js
+++ app.js
@@ -2,22 +2,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var helmet = require('helmet');
+var cors = require('cors')
 
 var mainRouter = require('./src/routes/MainRouter');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
 
-app.use(function(request, response, next) {
-    response.header("Access-Control-Allow-Origin", "*");
-    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
 app.use('/', mainRouter);
 
