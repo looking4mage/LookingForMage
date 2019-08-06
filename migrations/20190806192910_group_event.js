0a522622965a3c5aba4240049422cb8efd17f00c
Mateusz Gibula <gibulam94@gmail.com>
2019-08-06 20:05:04 +0200

    fix

--- migrations/20190806192910_group_event.js
+++ migrations/20190806192910_group_event.js
@@ -3,7 +3,7 @@ exports.up = function(knex) {
     return knex.schema.createTable('group_event',(table)=>{
         table.increments('id')
         table.timestamp('created_at').defaultTo(knex.fn.now())
-        table.timestamp('planned_to')
+        table.timestamp('planned_to').defaultTo(knex.fn.now())
         table.string('name')
         table.integer('group_id').unsigned().notNullable()//Jaka gra
         table.foreign('group_id').references('id').inTable('group')
